bb7060e2aa7730d354d7c6e55e2484f5420f720b
Jos van Goor <josvangoor@gmail.com>
2020-10-03 14:53:02 +0200

    finally some control over tls

--- app.py
+++ app.py
@@ -3,21 +3,8 @@ from random import randint
 
 SUMO_BINARY = "/usr/bin/sumo-gui"
 SUMO_COMMAND = [SUMO_BINARY, "-c", "SUMO/intersection.sumocfg"]
-end_nodes = \
-[
-    "edge_center_north",
-    "edge_center_east",
-    "edge_center_south",
-    "edge_center_west"
-]
-
-start_nodes = \
-[
-    "edge_north_center",
-    "edge_east_center",
-    "edge_south_center",
-    "edge_west_center"
-]
+end_nodes = ["edge_center_north", "edge_center_east", "edge_center_south", "edge_center_west"]
+start_nodes = ["edge_north_center", "edge_east_center", "edge_south_center", "edge_west_center"]
 numroutes = 0
 
 def generate_routes():
@@ -30,29 +17,48 @@ def generate_routes():
             traci.route.add("trip_{}".format(numroutes), [start_nodes[start], end_nodes[end]])
             numroutes += 1
 
-def get_traffic_lights(name):
+def close_empty_lanes():
+    # lanes = traci.trafficlight.getControlledLanes("tls_center")
+    links = traci.trafficlight.getControlledLinks("tls_center")
+    # state = traci.trafficlight.getRedYellowGreenState("tls_center")
 
-    pass
+    tls = [None] * 32
+    for idx in range(32):
+        tls[idx] = ["r", links[idx][0][0], links[idx][0][1], links[idx][0][2]]
+    
+    vehicles = traci.vehicle.getIDList()
+    for vehicle in vehicles:
+        lane = traci.vehicle.getLaneID(vehicle)
 
+        for idx in range(32):
+            if tls[idx][1] == lane:
+                tls[idx][0] = "g"
 
+    states = ""
+    for idx in range(32):
+        states += tls[idx][0]
+    print("states: {}".format(states))
+    traci.trafficlight.setRedYellowGreenState("tls_center", states)
 
-try:
-    traci.start(SUMO_COMMAND)
-    step = 0
+# try:
+traci.start(SUMO_COMMAND)
+step = 0
 
-    generate_routes()
+generate_routes()
 
-    print(traci.trafficlight.getPhase("tls_center"))
-    print(traci.trafficlight.getRedYellowGreenState("tls_center"))
+print(traci.trafficlight.getPhase("tls_center"))
 
-    while step < 100000:
-        if traci.simulation.getMinExpectedNumber() == 0:
-            traci.vehicle.add("newVeh", "trip_{}".format(randint(0, numroutes - 1)))
+while step < 10000:
+    if traci.simulation.getMinExpectedNumber() == 0:
+        traci.vehicle.add("newVeh", "trip_{}".format(randint(0, numroutes - 1)))
+        traci.vehicle.add("newVeh2", "trip_{}".format(randint(0, numroutes - 1)))
+    
+    close_empty_lanes()
 
-        traci.simulationStep()
-        step += 1
+    traci.simulationStep()
+    step += 1
 
-except Exception as e:
-    print("Caught exception: {}".format(e))
+# except Exception as e:
+#     print("Caught exception: {}".format(e))
 
 traci.close()
