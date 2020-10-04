f284288b8ec56584eca4060e57f5324738c91397
caitlin <hello@hello>
2020-10-04 10:43:40 +0200

    this works

--- beta_version/SUMOsim.py
+++ beta_version/SUMOsim.py
@@ -5,24 +5,31 @@ import sys
 import numpy as np
 import optparse
 
-if 'SUMO_HOME' in os.environ:
-    sys.path.append(os.path.join(os.environ.get("SUMO_HOME"), 'tools'))
-else:
-    sys.exit("please declare environment variable 'SUMO_HOME'")
+from tlscontroller import TlsController
 
-import traci
-from sumolib import checkBinary  # noqa
-optParser = optparse.OptionParser()
-optParser.add_option("--nogui", action="store_true",default=False, help="run the commandline version of sumo")
-options, args = optParser.parse_args()
-if options.nogui:
-    sumoBinary = checkBinary('sumo')
-else:
-    sumoBinary = checkBinary('sumo-gui')
 
-    # first, generate the route file for this simulation
-    #generate_routefile()
+if __name__ == "__main__":
+    if 'SUMO_HOME' in os.environ:
+        sys.path.append(os.path.join(os.environ.get("SUMO_HOME"), 'tools'))
+    else:
+        sys.exit("please declare environment variable 'SUMO_HOME'")
 
-    # this is the normal way of using traci. sumo is started as a
-    # subprocess and then the python script connects and runs
-traci.start([sumoBinary, "-c","Dutch.sumocfg"])
+    import traci
+    from sumolib import checkBinary  # noqa
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+        # first, generate the route file for this simulation
+        #generate_routefile()
+
+        # this is the normal way of using traci. sumo is started as a
+        # subprocess and then the python script connects and runs
+    traci.start([sumoBinary, "-c","intersection.sumocfg"])
+
+
+
