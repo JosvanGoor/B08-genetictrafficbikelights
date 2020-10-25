3f765cb53ece9735a19596d6460ab6d2fc23f57f
vladiftime <vladiftime60@gmail.com>
2020-10-25 16:16:46 +0100

    Genetic

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from tlscontroller import TlsController
 if 'SUMO_HOME' in os.environ :
     SUMO_BINARY = checkBinary('sumo-gui')       # for Win
 else :
-    SUMO_BINARY = "/usr/bin/sumo"           # for Unix
+    SUMO_BINARY = "/usr/bin/sumo"               # for Unix
 
 SUMO_COMMAND = [SUMO_BINARY, "-c", "quadintersection/quad.sumocfg"]
 numroutes = 0
